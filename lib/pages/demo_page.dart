40342954a429fddc737c496a061d4f4051aa32eb
Zujaj <rules.asia@gmail.com>
2021-02-09 19:21:12 +0500

    Updated Demo Page Code

--- lib/pages/demo_page.dart
+++ lib/pages/demo_page.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:multiple_selection_dialogue_app/widgets/multi_select_dialog.dart';
+import 'package:multiple_selection_dialogue_app/widgets/multi_select_form_field.dart';
 
 /// A demo page that displays an [ElevatedButton]
 class DemoPage extends StatelessWidget {
+  /// A Global Key to track the form's state
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    /// Stores the selected flavours
-    List<String> flavours = [];
-
-    return ElevatedButton(
-        child: Text('Flavours'),
-        onPressed: () async {
-          flavours = await showDialog<List<String>>(
-                  context: context,
-                  builder: (_) => MultiSelectDialog(
-                          question: Text('Select Your Flavours'),
-                          answers: [
-                            'Chocolate',
-                            'Caramel',
-                            'Vanilla',
-                            'Peanut Butter'
-                          ])) ??
-              [];
+    return Form(
+      key: _formKey,
+      child: MultiSelectFormField(
+        context: context,
+        buttonText: 'FLAVOURS',
+        itemList: ['Chocolate', 'Caramel', 'Vanilla', 'Peanut Butter'],
+        questionText: 'Select Your Flavours',
+        validator: (flavours) =>
+            flavours.length == 0 ? 'Please select at least one flavor!' : null,
+        onSaved: (flavours) {
           print(flavours);
           // Logic to save selected flavours in the database
-        });
+        },
+      ),
+      onChanged: () {
+        if (_formKey.currentState.validate()) {
+          // Invokes the OnSaved Method
+          _formKey.currentState.save();
+        }
+      },
+    );
   }
 }
